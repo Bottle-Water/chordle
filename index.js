36f88f38182b167e9a1ab405b301b633388524fb
Bottle-Water <78448678+Bottle-Water@users.noreply.github.com>
2024-04-12 18:17:46 -0400

    adding guess tracker

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const inputPiano = new Tone.Synth().toDestination();
 const chordPiano = new Tone.PolySynth(Tone.Synth).toDestination();
 const keys = document.querySelectorAll('.key');
-
+const game = document.getElementById('game');
 let selectedKeys = []
 let activeCount = 0
 const maxCount = 4
+
+drawGrid(game);
+
+const state = {
+    grid: Array(6)
+        .fill()
+        .map(() => Array(maxCount).fill('')),
+    currentRow: 0,
+    currentCol: 0,
+};
+
+
+
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
@@ -34,8 +47,7 @@ function playNote(key) {
 
 function submitGuess()
 {
-
-    // Todo: Make sure that you have picked the maxCount of keys
+    state.currentCol = 0;
     const keys = document.querySelectorAll('.active');
     if (keys.length < maxCount)
     {
@@ -54,6 +66,17 @@ function submitGuess()
         
         setTimeout(function() {
             key.classList.add('play');
+            console.log(`${state.currentRow} is state row and i is ${i}`)
+            
+            // Strip the octave
+            note = key.dataset.note;
+            note = note.substring(0, note.length-1)
+
+            state.grid[state.currentRow][state.currentCol] = note;
+            console.log(state.grid);
+            state.currentCol++;
+            updateGrid();
+            
           }, timeSkewMs);
         
         chordPiano.triggerAttack(key.dataset.note, now + timeSkew);
@@ -67,7 +90,9 @@ function submitGuess()
         keys.forEach(key => {
             key.classList.remove('active');
             key.classList.remove('play');
+            
         })
+        state.currentRow++;
     }, 2 * 1000);
 
     activeCount = 0;
@@ -80,3 +105,46 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
+
+function drawBox(container, row, col, note ='')
+{
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = `box${row}${col}`;
+    box.textContent = note;
+
+    container.appendChild(box);
+    return box;
+}
+
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for (let i = 0; i < 6; i++)
+    {
+        for (let j = 0; j < maxCount; j++)
+        {
+            drawBox(grid, i, j);
+        }
+    }
+    container.appendChild(grid);
+}
+
+function updateGrid() {
+    for (let i = 0; i < state.grid.length; i++)
+    {
+        for (let j = 0; j < maxCount; j++)
+        {
+            
+            const box = document.getElementById(`box${i}${j}`);
+            console.log(`On box${i}${j}`)
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
+state.grid = Array(6)
+    .fill()
+    .map(() => Array(maxCount).fill(""));
+updateGrid();
