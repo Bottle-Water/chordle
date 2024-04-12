7718b95321119d145ca99ad83adc11d954f4eb4c
Bottle-Water <78448678+Bottle-Water@users.noreply.github.com>
2024-04-12 15:46:04 -0400

    limits to chord selection (maxCount)

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ const inputPiano = new Tone.Synth().toDestination();
 const keys = document.querySelectorAll('.key');
 
 let selectedKeys = []
+let activeCount = 0
+const maxCount = 4
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
 })
 
 function playNote(key) {
-    
-    pitch = key.dataset.note;
-    inputPiano.triggerAttackRelease(pitch, "8n");
+    // Toggle active
+    console.log(activeCount);
+    if (key.classList.contains('active'))
+    {
+        key.classList.remove('active');
+        activeCount--;
+    }
+    else if (activeCount < 4)
+    {
+        key.classList.add('active');
+        activeCount++;
+        pitch = key.dataset.note;
+        inputPiano.triggerAttackRelease(pitch, "8n");
+    }
+    else
+    {
+        inputPiano.triggerAttackRelease("C1", "32n");
+    }
+
 }
