8b972513fcf6b21f782ff258890cf9a00584a0b5
Zach Burkett <78448678+Bottle-Water@users.noreply.github.com>
2024-04-17 09:29:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ let activeCount = 0;
 let rightCount = 0;
 let str = "";
 let copyStr = "";
-let tempAnswerPlayer = ["F3","Ab3", "C4", "F4"];
+let tempAnswerPlayer = ["G3","B3", "D4", "Gb4"];
 const maxCount = 4
 
 drawGrid(game);
